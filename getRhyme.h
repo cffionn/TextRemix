79bd3280d4d77c51ac1f657b6c9fd234be3a85e1
Chris McGinn <cfmcginn@mit.edu>
2016-05-02 13:28:06 +0200

    Added rhyme parser for interactive creation of database

--- getRhyme.h
+++ getRhyme.h
@@ -12,47 +12,73 @@
 #include "defAlphaNumSoup.h"
 #include "returnFileList.h"
 
+const std::string rhymeDatabasePath = "rhymeDatabase/";
+
 int getRhyme(std::string inWord, std::vector<std::string>* fileList_p)
 {
   boost::algorithm::to_lower(inWord);
 
-  int syllable = -1;
+  int rhymeNum = -1;
 
-  for(int sylIter = 0; sylIter < nSylFiles; sylIter++){
-    std::ifstream file(sylFileStrings[sylIter].c_str());
+  const int nRhymeFiles = (int)fileList_p->size();
+
+  for(int rhymeIter = 0; rhymeIter < nRhymeFiles; rhymeIter++){
+    std::ifstream file(fileList_p->at(rhymeIter).c_str());
     std::string str;
     bool breakBool = false;
 
     while(std::getline(file, str)){
       boost::algorithm::to_lower(str);
       if(!strcmp(inWord.c_str(), str.c_str())){
-	syllable = sylIter+1;
+	rhymeNum = rhymeIter;
 	breakBool = true;
 	break;
       }
     }
     file.close();    
     if(breakBool) break;
-  }  
+  }
 
-  while(syllable == -1){
-    std::cout << "Word not found. Please enter integer corresponding to number of syllables (1-8) in \'" << inWord << "\': ";
+  while(rhymeNum == -1){
+    std::cout << "Word not found. Please enter integer corresponding to rhyme file (0-" << nRhymeFiles-1 << ") of \'" << inWord << "\'. If no rhyme file yet exists, enter -999: " << std::endl;
+
+    for(int rhymeIter = 0; rhymeIter < nRhymeFiles; rhymeIter++){
+      std::string rhymingWord =  fileList_p->at(rhymeIter).substr(0, fileList_p->at(rhymeIter).find(".txt"));
+      rhymingWord = rhymingWord.substr(rhymingWord.find("/")+1, rhymingWord.find("/")+1-rhymingWord.size());
+
+      std::cout << " Input " << rhymeIter << ": " << rhymingWord << std::endl;
+    }
+
+    std::cout << " Enter num: ";
     int input;
     std::cin >> input;
-    if(std::cin.fail()){
-      std::cout << "Input was not integer. Please enter integer 1 thru 8." << std::endl;
+    if(!(std::cin.good())){
+      std::cout << "Input was not integer. Please enter integer 0 thru " << nRhymeFiles-1 << ", or -999 if no match exists." << std::endl;
+      std::cin.clear();
+      std::cin.ignore(INT_MAX, '\n');
     }
-    else if(input < 1) std::cout << "Input less than 1. Please enter integer 1 thru 8." << std::endl;
-    else if(input > 8) std::cout << "Input greater than 8. Please enter integer 1 thru 8." << std::endl;
-    else{
-      std::fstream file(sylFileStrings[input-1].c_str(), std::fstream::app);
+    else if(input < 0 && input != -999) std::cout << "Input less than 0, not -999. Please enter integer 0 thru " << nRhymeFiles-1 << ", or -999 if no match exists." << std::endl;
+    else if(input > nRhymeFiles-1) std::cout << "Input greater than " << nRhymeFiles-1 << ". Please enter integer 0 thru "<< nRhymeFiles-1 <<", or -999 if no match exists." << std::endl;
+    else if(input == -999){
+      std::cout << "No file yet exists. File \'" << inWord << ".txt\' will be created." << std::endl;
+      
+      std::string newFileName = rhymeDatabasePath + inWord + ".txt";
+      std::ofstream file(newFileName.c_str());
       file << inWord << std::endl;
       file.close();
-      syllable = input;
+      fileList_p->push_back(newFileName);
+
+      rhymeNum = (int)(fileList_p->size()-1);
+    }
+    else{
+      rhymeNum = input;
+      std::fstream file(fileList_p->at(rhymeNum).c_str(), std::fstream::app);
+      file << inWord << std::endl;
+      file.close();
     }			      
   }
   
-  return syllable;
+  return rhymeNum;
 }
 
 #endif
---
+++ testFunctions/testGetRhyme.C
@@ -0,0 +1,33 @@
+#include "returnFileList.h"
+#include "getRhyme.h"
+#include <iostream>
+
+const std::string filterStr = ".txt";
+
+int testGetRhyme(const std::string inWord)
+{
+  std::vector<std::string> testFileVect = returnFileList(rhymeDatabasePath, filterStr);
+  std::cout << "Number of files (filter == \'" << filterStr << "\'): " << testFileVect.size() << std::endl;
+
+  for(int iter = 0; iter < (int)testFileVect.size(); iter++){
+    std::cout << " File #" << iter << ": " << testFileVect.at(iter) << std::endl;
+  }
+
+  return getRhyme(inWord, &testFileVect);
+}
+
+int main(int argc, char *argv[])
+{
+  if(argc != 2){
+    std::cout << "Usage: testGetRhyme.exe <inputWord>" << std::endl;
+    std::cout << "Number of args given: " << argc << std::endl;
+    for(int iter = 0; iter < argc; iter++){
+      std::cout << "  argv[" << iter << "]: " << argv[iter] << std::endl;
+    }
+    return -1;
+  }
+
+  int retVal = testGetRhyme(argv[1]);
+  std::cout << "Rhyme pos: " << retVal << std::endl;
+  return retVal;
+}
