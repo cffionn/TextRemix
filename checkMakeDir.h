6dd98a130a67551439bb37e1fb59572595c8984b
Chris McGinn <cfmcginn@mit.edu>
2016-05-02 09:33:39 +0200

    Updated checkMakeDir.h

--- checkMakeDir.h
+++ checkMakeDir.h
@@ -8,9 +8,27 @@
 
 struct stat st = {0};
 
+bool checkDir(const std::string inPath)
+{
+
+  if(stat(inPath.c_str(), &st) != 0) return false;
+
+  if(st.st_mode & S_IFDIR) return true;
+  else return false;
+}
+
+bool checkFile(const std::string inFile)
+{
+  if(stat(inFile.c_str(), &st) != 0) return false;
+
+  if(st.st_mode & S_IFREG) return true;
+  else return false;
+}
+
+
 void checkMakeDir(const std::string inPath)
 {
-  if(stat(inPath.c_str(), &st) == -1) {
+  if(!checkDir(inPath)){
     mkdir(inPath.c_str(), 0700);
   }
 
