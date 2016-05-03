dd7fd598abfe2c19655f9d9f9714f95df22e9cfa
Chris McGinn <cffionn@gmail.com>
2016-05-03 13:16:00 +0200

    Added a bunch of cleaning functions

--- cleanAllBackup.sh
+++ cleanAllBackup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+rm $PWD/*.C~
+rm $PWD/testFunctions/*.C~
+rm $PWD/*.h~
+rm $PWD/interface/*.h~
+rm $PWD/*.sh~
 rm $PWD/inputDatabase/*.txt~
 rm $PWD/rhymeDatabase/*.txt~
 rm $PWD/syllableDatabase/*.txt~
---
+++ cleanExecutables.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm $PWD/*.exe
+rm $PWD/testFunctions/*.exe
