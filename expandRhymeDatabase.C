9ad46fbbfb17a07cd0115b3dbe51872f93b51efd
Chris McGinn <cffionn@gmail.com>
2016-05-09 19:04:50 +0200

    added filename query for new file

--- expandRhymeDatabase.C
+++ expandRhymeDatabase.C
@@ -119,8 +119,11 @@ int expandRhymeDatabase()
 
     if((input.find("y") != std::string::npos && input.size() == 1) || (input.find("yes") != std::string::npos && input.size() == 3)){
       //yes block
-      
-
+      std::string newFileName = wordStrVect_p->at(wordIter).substr(0, 1);
+      boost::algorithm::to_upper(newFileName);
+      if(wordStrVect_p->at(wordIter).size() > 1) newFileName = newFileName + wordStrVect_p->at(wordIter).substr(1, wordStrVect_p->at(wordIter).size()-1);
+      newFileName = wordLastStr + "_" + newFileName + ".txt";
+      std::cout << "Filename \'" << newFileName << "\' ok? (y/n)" << std::endl;
     }
     else if((input.find("n") != std::string::npos && input.size() == 1) || (input.find("no") != std::string::npos && input.size() == 2)){
       //no block
