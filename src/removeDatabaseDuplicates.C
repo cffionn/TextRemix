519def01516979e3f683de09a48a7e3c08da2498
Chris McGinn <cffionn@gmail.com>
2016-05-15 21:05:21 +0200

    remove duplicates successfully lists duplicates within separate files

--- src/removeDatabaseDuplicates.C
+++ src/removeDatabaseDuplicates.C
@@ -29,8 +29,55 @@ int removeDatabaseDuplicates(const std::string dirPath)
 
   const std::string filterString = ".txt";
   std::vector<std::string> dbFileList = returnFileList(dirPath, filterString);
-  int nFiles = (int)dbFileList.size();
+  const int nFiles = (int)dbFileList.size();
 
+  for(int fileIter = 0; fileIter < nFiles; fileIter++){
+    std::ifstream file1(dbFileList.at(fileIter));
+    std::string inStr1;
+    std::vector<std::string>* inStrVect1_p = new std::vector<std::string>;
+
+    while(std::getline(file1, inStr1)){
+      if(inStr1.size() == 0) continue;
+      inStrVect1_p->push_back(inStr1);
+    }
+    file1.close();
+
+    for(int fileIter2 = fileIter+1; fileIter2 < nFiles; fileIter2++){
+      std::ifstream file2(dbFileList.at(fileIter2));
+      std::string inStr2;
+      std::vector<std::string>* inStrVect2_p = new std::vector<std::string>;
+
+      while(std::getline(file2, inStr2)){
+	if(inStr2.size() == 0) continue;
+	inStrVect2_p->push_back(inStr2);
+      }      
+      file2.close();
+
+      int wordIter1 = 0;
+      while(wordIter1 < (int)inStrVect1_p->size()){
+	std::string word1 = inStrVect1_p->at(wordIter1);
+
+	int wordIter2 = 0;
+	while(wordIter2 < (int)inStrVect2_p->size()){
+	  std::string word2 = inStrVect2_p->at(wordIter2); 
+	  
+	  if(word1.size() == word2.size() && word1.find(word2) != std::string::npos){
+	    std::cout << "Duplicate word \'" << word1 << "\' in files \'" << dbFileList.at(fileIter) << "\', \'" << dbFileList.at(fileIter2) << "\'." << std::endl;
+	  }
+	  wordIter2++;
+	}
+
+	wordIter1++;
+      }
+
+      inStrVect2_p->clear();
+      delete inStrVect2_p;
+    }    
+
+    inStrVect1_p->clear();
+    delete inStrVect1_p;
+  }
+  
 
   retVal = 0;
   return retVal;
