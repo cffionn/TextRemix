80d710e2b2695d55be2ce613d99b5f85c3f3fe83
Chris McGinn <cffionn@gmail.com>
2016-05-16 15:22:06 +0200

    Almost have removal functionality; looking into merger

--- src/removeDatabaseDuplicates.C
+++ src/removeDatabaseDuplicates.C
@@ -10,7 +10,7 @@
 
 #include <boost/algorithm/string.hpp>
 
-int removeDatabaseDuplicates(const std::string dirPath)
+int removeDatabaseDuplicates(std::string dirPath)
 {
   int retVal = -1;
 
@@ -42,6 +42,8 @@ int removeDatabaseDuplicates(const std::string dirPath)
     }
     file1.close();
 
+    bool file1IsModded = false;
+
     for(int fileIter2 = fileIter+1; fileIter2 < nFiles; fileIter2++){
       std::ifstream file2(dbFileList.at(fileIter2));
       std::string inStr2;
@@ -52,6 +54,8 @@ int removeDatabaseDuplicates(const std::string dirPath)
 	inStrVect2_p->push_back(inStr2);
       }      
       file2.close();
+
+      bool file2IsModded = false;
 
       int wordIter1 = 0;
       while(wordIter1 < (int)inStrVect1_p->size()){
@@ -64,7 +68,7 @@ int removeDatabaseDuplicates(const std::string dirPath)
 	  if(word1.size() == word2.size() && word1.find(word2) != std::string::npos){
 	    std::cout << "Duplicate word \'" << word1 << "\' in files \'" << dbFileList.at(fileIter) << "\', \'" << dbFileList.at(fileIter2) << "\'." << std::endl;
 
-	    
+	    bool continueBool = false;
 	    int inputInt = -1;
 	    while(inputInt == -1){
 	      std::cout << " Remove word \'" << word1 << "\' from file \'" << dbFileList.at(fileIter) << "\'? (y/n)";
@@ -74,23 +78,84 @@ int removeDatabaseDuplicates(const std::string dirPath)
 	      inputInt = parseYesNo(input);
 
 	      if(inputInt == 1){
-		std::cout << "YES!" << std::endl;
-	      }
-	      else if(inputInt == 0){
-		std::cout << "No?" << std::endl;
+		file1IsModded = true;
+		inStrVect1_p->erase(inStrVect1_p->begin()+wordIter1);
+		wordIter2 = (int)inStrVect2_p->size();
+		continueBool = true;
 	      }
 	    }
 
+	    if(continueBool) continue;
+            inputInt = -1;
+            while(inputInt == -1){
+	      std::cout << " Remove word \'" << word1 << "\' from file \'" << dbFileList.at(fileIter2) << "\'? (y/n)";
+	      std::string input;
+	      std::cin >> input;
+
+              inputInt = parseYesNo(input);
+
+              if(inputInt == 1){
+		file2IsModded = true;
+		inStrVect2_p->erase(inStrVect2_p->begin()+wordIter2);
+                continueBool = true;
+              }
+            }
+
+	    if(continueBool) continue;
+	    inputInt = -1;
+            while(inputInt == -1){
+	      std::cout << "  Keep word \'" << word1 << "\' in both file \'" << dbFileList.at(fileIter) << "\' and file \'" << dbFileList.at(fileIter2) << "\'? (y/n)";
+	      std::string input;
+	      std::cin >> input;
+
+              inputInt = parseYesNo(input);
+
+              if(inputInt == 1){
+		std::cout << "YES!" << std::endl;
+		wordIter2++;
+                continueBool = true;
+              }
+            }
+	    if(continueBool) continue;
+
 	  }
-	  else wordIter2++;
+	  else wordIter2++;	  
 	}
 
 	wordIter1++;
       }
 
+      if(file2IsModded){
+	std::string outFile2Name = "test_" + dbFileList.at(fileIter2).substr(dbFileList.at(fileIter2).find("/")+1, dbFileList.at(fileIter2).size() - dbFileList.at(fileIter2).find("/")-1);
+	
+	if(dirPath.substr(dirPath.size()-1, 1).find("/") == std::string::npos) dirPath = dirPath + "/";
+	outFile2Name = dirPath + outFile2Name;
+	
+	std::ofstream outFile2(outFile2Name);
+	
+	for(int wordIter2 = 0; wordIter2 < (int)inStrVect2_p->size(); wordIter2++){
+	  outFile2 << inStrVect2_p->at(wordIter2) << std::endl;
+	}
+
+	outFile2.close();
+      }
+
       inStrVect2_p->clear();
       delete inStrVect2_p;
     }    
+    
+    if(file1IsModded){
+      std::string outFile1Name = "test_" + dbFileList.at(fileIter).substr(dbFileList.at(fileIter).find("/")+1, dbFileList.at(fileIter).size() - dbFileList.at(fileIter).find("/")-1);
+      if(dirPath.substr(dirPath.size()-1, 1).find("/") == std::string::npos) dirPath = dirPath + "/";
+      outFile1Name = dirPath + outFile1Name;
+      std::ofstream outFile1(outFile1Name);
+      
+      for(int wordIter1 = 0; wordIter1 < (int)inStrVect1_p->size(); wordIter1++){
+	outFile1 << inStrVect1_p->at(wordIter1) << std::endl;
+      }
+      
+      outFile1.close();
+    }      
 
     inStrVect1_p->clear();
     delete inStrVect1_p;
